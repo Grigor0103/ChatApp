6776415496461b5f64f514b999e836fbaf44b8c4
Grigor0103 <45464995+Grigor0103@users.noreply.github.com>
2019-06-21 15:30:32 +0400

    start

---
+++ ChatApp/ChatApp/Message.swift
@@ -0,0 +1,35 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Administrator on 5/20/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Message: NSObject {
+    
+    var fromId: String?
+    var text: String?
+    var timestamp: NSNumber?
+    var toId: String?
+    var imageUrl: String?
+    var imageWidth: NSNumber?
+    var imageHeight: NSNumber?
+    init(dictionary: [String: Any]) {
+        self.fromId = dictionary["fromId"] as? String
+        self.text = dictionary["text"] as? String
+        self.toId = dictionary["toId"] as? String
+        self.timestamp = dictionary["timestamp"] as? NSNumber
+        self.imageUrl = dictionary["imageUrl"] as? String
+        self.imageHeight = dictionary["imageHeight"] as? NSNumber
+        self.imageWidth = dictionary["imageWidth"] as? NSNumber
+    }
+    
+    func chatPartnerId() -> String? {
+        return fromId == Auth.auth().currentUser?.uid ? toId : fromId
+    }
+    
+}
